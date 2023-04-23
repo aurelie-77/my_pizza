b2f7683b95324fdd2e0a4b86dcc17dcd8652f658
laugeois aurelie <alaulouann@gmail.com>
2023-04-23 15:14:54 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,11 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my pizza': {
+        'ingredients': {'mozzarella': 3, 'garlic': 5, 'onion': 3},
+        'price': 11,
+    },
+    
+    
 }
